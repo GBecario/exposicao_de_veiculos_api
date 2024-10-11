667713f2907d7cf1fe3941a96f2e8a5909d39194
GBecario <gustavobecario1@gmail.com>
2024-10-11 13:56:10 -0300

    first commit

---
+++ src/main/java/br/com/exposicao/ExposicaoDeVeiculosApplication.java
@@ -0,0 +1,13 @@
+package br.com.exposicao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExposicaoDeVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExposicaoDeVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/exposicao/ExposicaoDeVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exposicao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExposicaoDeVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
